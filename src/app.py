f506948a70c19eb5f6535044db9cd3863ccda083
Jonathan <jonyoner@gmail.com>
2025-08-06 10:19:45 +0000

    añado el enlace a la app front-end

--- src/app.py
+++ src/app.py
@@ -47,6 +47,15 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
 
 @app.route("/signup", methods=["POST"])
 def signup():
